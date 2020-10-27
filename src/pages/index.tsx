27da4c7b4321fb195a77674998786bfef2c44bd4
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2020-10-27 12:32:39 +0100

    add favicon and basic links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,83 @@
 import Head from "next/head";
 
 export default function Home() {
+  const name = "Matteo Pagliazzi";
   return (
-    <main className="container mx-auto px-4 py-4">
+    <main
+      className="
+        w-full  bg-gray-200 text-gray-900
+        min-h-screen flex flex-col
+         px-12 py-12 pb-2"
+    >
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{name}</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
-      <header className="prose prose-xl">
-        <h1>Matteo Pagliazzi</h1>
+      <header className="mb-4">
+        <p className="prose prose-xl max-w-full">
+          <h1
+            className="border-b-4 border-blue-500 pb-4 w-full"
+            style={{ fontWeight: "bold" }}
+          >
+            {name}
+          </h1>
+        </p>
       </header>
-      <article className="prose prose-xl">
-        <ul>
-          <li>
-            <a href="">Curriculum Vitae / Resume</a>
-          </li>
-          <li>
-            <a href="">Github</a>
-          </li>
-          <li>
-            <a href="">Linkedin</a>
-          </li>
-          <li>
-            <a href="">Email</a>
-          </li>
-          <li>
-            <a href="">Twitter</a>
-          </li>
-        </ul>
+      <article className="flex-grow prose">
+        <p className="lead">
+          <ul>
+            <li>
+              <a href="">Curriculum Vitae / Resume</a>
+            </li>
+            <li>
+              <a href="https://github.com/paglias" target="_blank">
+                Github
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/matteopagliazzi/"
+                target="_blank"
+              >
+                Linkedin
+              </a>
+            </li>
+            <li>
+              <a href="mailto:matteopagliazzi@gmail.com" target="_blank">
+                Email
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/MatteoPagliazzi" target="_blank">
+                Twitter
+              </a>
+            </li>
+          </ul>
+        </p>
       </article>
 
-      <footer className="prose">
-        <p>Copyright</p>
+      <footer className="">
+        <p className="prose">
+          <small>&copy; 2020 - Matteo Pagliazzi</small>
+        </p>
       </footer>
     </main>
   );
